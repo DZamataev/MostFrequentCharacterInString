0701e56e6534b9d857be5c932838495f74595284
Denis Zamataev <zamataev@corp.sputnik.ru>
2015-07-06 18:03:03 +0300

    Initial Commit

---
+++ MostFrequentCharacter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MostFrequentCharacter
+//
+//  Created by Denis Zamataev on 06/07/15.
+//  Copyright (c) 2015 Mysterious Organization. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
